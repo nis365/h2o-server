36d3b630e55c37428ddaae1e454a7b83b5390242
shilwu <shilwu@cisco.com>
2020-03-18 15:30:43 +0800

    test entry file

--- docker-init.sh
+++ docker-init.sh
@@ -7,4 +7,4 @@
 # Check that we can at least run H2O with the given java.
 # java -jar h2o.jar -version
 
-java -Xmx2g -jar /root/h2o-3.28.0.3/h2o.jar -name H2ONode  -port 54321
+java -Xmx2g -jar /tmp/h2o-3.28.0.3/h2o.jar -name H2ONode  -port 54321
